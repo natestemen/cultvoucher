dc2dd1fdca16b1ebe04dfc301d75b3613ffdbcb1
Nate Stemen <nate.stemen@overleaf.com>
2021-11-01 08:38:47 -0400

    delete frames if they are the same as previous one
    not only does this create a smoother looking gif, but it's smaller as well!

--- gif.sh
+++ gif.sh
@@ -6,20 +6,23 @@ png_dir=pngs
 mkdir $png_dir
 cp .latexmkrc latexmkrc
 
-INDEX=0
+INDEX=1
 for commit in $(git rev-list --reverse main); do
     echo "###################################"
     echo `git log --format=%B -n 1 $commit`
     echo "###################################"
     git checkout $commit
     latexmk -jobname=$commit main.tex
-    convert -density 100 -background white -alpha off ${commit}.pdf ${commit}.png
+    convert -density 75 -background white -alpha off ${commit}.pdf ${commit}.png
     printf -v formatted_index "%05d" ${INDEX} # TODO: unlikely to have >99,999 commits, but...
     convert +append *.png ./${png_dir}/${formatted_index}.png
     rm ${commit}*
 
     let INDEX=${INDEX}+1
 done
+
+git checkout main
+rm latexmkrc
 
 xdim=()
 ydim=()
@@ -41,9 +44,28 @@ for png in ./${png_dir}/*; do
     convert -gravity west -extent "${xmax} x ${ymax}" ${png} ${png}
 done
 
-# TODO: check if sequential images are the same with
-# convert pngs/00000.png pngs/00001.png -metric RMSE -compare -format "%[distortion]" info:
+to_delete=()
+for png in ${png_dir}/*; do
+    if [[ " ${to_delete[*]:-()} " =~ " ${png} " ]]; then
+        continue
+    fi
 
-convert -delay 10 -loop 0 pngs/*.png anim.gif
+    int=$(echo $png | egrep '[1-9]\d*' --only-matching)
+    next=$((int+1))
+    printf -v formatted_int "%05d" ${next}
+    next_png="./${png_dir}/${formatted_int}.png"
+    if [ ! -f "$next_png" ]; then
+        continue
+    fi
+    
+    diff=$(convert ${png} ${next_png} -metric RMSE -compare -format "%[distortion]" info:)
+    if (( $(echo "$diff == 0" | bc -l) )); then
+        to_delete+=("${next_png}")
+    fi
+done
 
-git checkout main
+for png in "${to_delete[*]}"; do
+    rm $png
+done
+
+convert -delay 20 -loop 0 pngs/*.png anim.gif
