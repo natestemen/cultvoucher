957fa5ea325f7bdf6d5dcfe5b384f7d15c96c5df
Nate Stemen <nate.stemen@overleaf.com>
2021-11-14 00:59:47 -0500

    add some nice pretty printing
    also use `-colorspace RGB` so that convert stops
    complaining

--- gif.sh
+++ gif.sh
@@ -6,14 +6,21 @@ png_dir=pngs
 mkdir $png_dir
 cp .latexmkrc latexmkrc
 
+ITALIC='\033[0;3m'
+INVERT='\033[0;7m'
+NC='\033[0m'
+
+total_commits=`git rev-list --all --count`
 INDEX=1
 for commit in $(git rev-list --reverse main); do
-    echo "###################################"
-    echo `git log --format=%B -n 1 $commit`
-    echo "###################################"
-    git checkout $commit
-    latexmk -jobname=$commit main.tex
-    convert -density 75 -background white -alpha off ${commit}.pdf ${commit}.png
+    commit_message=`git log --format=%s -n 1 $commit`
+    commit_date=`git log --date=short --format=%cd -n 1 $commit`
+    echo -e "processing commit ${INVERT}$INDEX${NC} of $total_commits"
+    echo -e "commit message: ${ITALIC}${commit_message}${NC}"
+    echo "date: ${commit_date}"
+    git checkout $commit &> /dev/null
+    latexmk -jobname=$commit main.tex &> /dev/null
+    convert -density 75 -background white -alpha off -colorspace RGB ${commit}.pdf ${commit}.png
     printf -v formatted_index "%05d" ${INDEX} # TODO: unlikely to have >99,999 commits, but...
     convert +append *.png ./${png_dir}/${formatted_index}.png
     rm ${commit}*
@@ -27,7 +34,6 @@ rm latexmkrc
 xdim=()
 ydim=()
 for png in ./${png_dir}/*; do
-    echo "getting dimensions of ${png}"
     dimensions=`convert $png -format "%w x %h" info:`
     x=$(echo $dimensions | grep '^\d*' --only-matching)
     y=$(echo $dimensions | grep '\d*$' --only-matching)
@@ -44,6 +50,7 @@ for png in ./${png_dir}/*; do
     convert -gravity west -extent "${xmax} x ${ymax}" ${png} ${png}
 done
 
+echo "deleting duplicate pngs"
 to_delete=()
 for png in ${png_dir}/*; do
     if [[ " ${to_delete[*]:-()} " =~ " ${png} " ]]; then
@@ -68,4 +75,5 @@ for png in "${to_delete[*]}"; do
     rm $png
 done
 
+echo "creating final gif"
 convert -delay 20 -loop 0 pngs/*.png anim.gif
